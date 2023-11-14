849b648a0304218df4a6a4cac074e92b8ed9141f
kuredema7 <kuredema7@gmail.com>
2023-11-14 16:37:01 +0300

    refactor: add custom font `Proxima Nova`

--- app/src/main/java/com/example/cta_app/ui/theme/Type.kt
+++ app/src/main/java/com/example/cta_app/ui/theme/Type.kt
@@ -2,33 +2,45 @@ package com.example.cta_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.cta_app.R
 
-// Set of Material typography styles to start with
+val ProximaNova = FontFamily(
+    Font(R.font.proxima_nova_light, FontWeight.Light),
+    Font(R.font.proxima_nova_regular, FontWeight.Normal),
+    Font(R.font.proxima_nova_semibold, FontWeight.SemiBold),
+    Font(R.font.proxima_nova_bold, FontWeight.Bold)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ProximaNova,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = ProximaNova,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+    titleMedium = TextStyle(
+        fontFamily = ProximaNova,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.15.sp
+    ),
+    labelMedium = TextStyle(
+        fontFamily = ProximaNova,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
-    )
-    */
+    ),
 )
