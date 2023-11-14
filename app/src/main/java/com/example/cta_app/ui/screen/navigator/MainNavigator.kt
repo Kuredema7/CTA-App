5977dd3467012161dc21da22ca98dac9b6602f83
kuredema7 <kuredema7@gmail.com>
2023-11-14 11:33:59 +0300

    fix: optimize unused import files

--- app/src/main/java/com/example/cta_app/ui/screen/navigator/MainNavigator.kt
+++ app/src/main/java/com/example/cta_app/ui/screen/navigator/MainNavigator.kt
@@ -1,10 +1,8 @@
 package com.example.cta_app.ui.screen.navigator
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
