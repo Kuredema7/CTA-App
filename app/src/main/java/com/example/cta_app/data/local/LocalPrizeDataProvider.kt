be35beb58d519636cebf856b96577da9df6466ad
kuredema7 <kuredema7@gmail.com>
2023-11-14 14:28:14 +0300

    fix: add a local data source to fetch prize list

---
+++ app/src/main/java/com/example/cta_app/data/local/LocalPrizeDataProvider.kt
@@ -0,0 +1,32 @@
+package com.example.cta_app.data.local
+
+import com.example.cta_app.data.Prize
+
+object LocalPrizeDataProvider {
+    val prizes = listOf(
+        Prize(
+            mediaType = "Ilefx",
+            mediaPrice = 110.50
+        ),
+        Prize(
+            mediaType = "Portal USB",
+            mediaPrice = 30.00
+        ),
+        Prize(
+            mediaType = "Monitor F34D",
+            mediaPrice = 250.89
+        ),
+        Prize(
+            mediaType = "S6 ViewFinity Samsung",
+            mediaPrice = 459.99
+        ),
+        Prize(
+            mediaType = "Splash Gateway L8",
+            mediaPrice = 85.00
+        ),
+        Prize(
+            mediaType = "Fiber Opt.32",
+            mediaPrice = 20.99
+        ),
+    )
+}
