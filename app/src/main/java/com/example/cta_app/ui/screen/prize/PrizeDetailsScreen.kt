396ac4442df35c587e13873372e48a4143429fd3
moaliyou <moaliyou47@gmail.com>
2023-11-28 22:20:13 +0300

    feat: add filter chips to sort prize details list

--- app/src/main/java/com/example/cta_app/ui/screen/prize/PrizeDetailsScreen.kt
+++ app/src/main/java/com/example/cta_app/ui/screen/prize/PrizeDetailsScreen.kt
@@ -8,15 +8,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Sort
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -25,6 +30,9 @@ import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Transparent
@@ -43,6 +51,7 @@ fun PrizeDetailsScreen(
     lazyListState: LazyListState = rememberLazyListState()
 ) {
     val prizeDetailsUiState by prizeDetailsViewModel.uiState.collectAsState()
+    var isSelected by remember { mutableStateOf(false) }
 
     Column(modifier = modifier) {
         SearchBar(
@@ -55,6 +64,13 @@ fun PrizeDetailsScreen(
                 .fillMaxWidth()
                 .padding(dimensionResource(R.dimen.padding_medium))
         )
+        SortSection(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = dimensionResource(R.dimen.padding_medium)),
+            isSelected = isSelected,
+            onSortClick = { isSelected = !isSelected }
+        )
         PrizeDetailsList(
             prizeDetailsUiState = prizeDetailsUiState,
             lazyListState = lazyListState,
@@ -62,6 +78,54 @@ fun PrizeDetailsScreen(
                 .fillMaxSize()
                 .background(MaterialTheme.colorScheme.surface)
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SortSection(
+    modifier: Modifier = Modifier,
+    isSelected: Boolean,
+    onSortClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_extra_medium))
+        ) {
+            Icon(
+                Icons.Default.Sort,
+                contentDescription = "Sort Icon"
+            )
+            Text(
+                text = "Sort by",
+                style = MaterialTheme.typography.labelLarge,
+                color = MaterialTheme.colorScheme.outline
+            )
+        }
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_extra_medium))
+        ) {
+            FilterChip(
+                selected = isSelected,
+                onClick = onSortClick,
+                label = { Text(text = "A-Z") },
+                leadingIcon = if (isSelected) {
+                    {
+                        Icon(
+                            Icons.Default.Done,
+                            contentDescription = "Done Icon",
+                            modifier = Modifier.size(FilterChipDefaults.IconSize)
+                        )
+                    }
+                } else {
+                    null
+                }
+            )
+        }
     }
 }
 
